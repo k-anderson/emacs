36889831111cbc1e449ae42b11b566f3520468c7
Alan Castro <alanclic@gmail.com>
2010-05-31 21:56:48 -0300

    removed project defs from version control.

--- rc/emacs-rc-cpp-projects-defs.el
+++
@@ -1,8 +0,0 @@
-(setq booksample-project
-      (ede-cpp-root-project "booksamples"
-                            :file "/mnt/sda6/alan/workspace/c++/pthread_srcs/booksamples/CMakeLists.txt"
-                            :system-include-path '()
-                            :local-variables (list
-                                              (cons 'compile-command 'alexott/gen-cmake-debug-compile-string)
-                                              )
-                            ))
